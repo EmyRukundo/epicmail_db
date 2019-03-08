f7684d82447b36fd1eef245be8974a10ab57a3a0
EmyRukundo <rukundoemma@gmail.com>
2019-03-08 14:41:35 +0200

    update

---
+++ server/controllers/message.js
@@ -0,0 +1,161 @@
+import Database from '../db/db-connection';
+import Validation from '../helpers/validation';
+import messages from'../models/message.js';
+import joi from 'joi';
+
+
+//@get all messages
+const getMessages = async (req, res) => res.json({
+    status: 200,
+    data:await messages(),
+  });
+
+
+  //@Create message
+  const createMessage = (req, res) => {
+    joi.validate(req.body, Validation.messageSchema, Validation.validationOption, async (err, result) => {
+      if (err) {
+        return res.json({
+          status: 400,
+          error: err.details[0].message,
+        });
+      }
+      const dayMonthYear = result.happeningOn.split('/');
+      const date = new Date(dayMonthYear[2], dayMonthYear[1], dayMonthYear[0]);
+      const newMessage = [
+        id,
+        new Date(),
+        result.subject,
+        result.message,
+        result.senderId,
+        result.receivedId,
+        result.parentMessageId,
+        result.status,
+      ];
+      const sql = 'INSERT INTO meesage_table (id, created_on,subject,message,senderId,receiverId,parentMessageId,status) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *';
+      const messageSql =Database.executeQuery(sql, newMessage);
+      messageSql.then((insertedMessage) => {
+        if (insertedMessage.rows.length) {
+          return res.status(200).json({
+            status: 200,
+            data: insertedMessage.rows,
+          });
+        }
+  
+        return res.status(400).json({
+          status: 400,
+          error: " Cant not save data in the database",
+        });
+      });
+    }).catch((error) => {
+      res.status(500).json({
+        status: 500,
+        error: `Internal server error ${error}`,
+      });
+    });
+  };
+  
+
+//@get a specific Email
+   const specificEmail = (req, res) => {
+    const sql = `SELECT * FROM message_table WHERE id = '${req.params.id}'`;
+    const messageSql = Connection.executeQuery(sql);
+    messageSql.then((result) => {
+      if (result.rows.length) {
+        return res.status(200).json({
+          status: 200,
+          data: result.rows,
+        });
+      }
+  
+      return res.status(404).json({
+        status: 404,
+        error: 'No email found on this id',
+      });
+    }).catch(error => res.status(500).json({
+      status: 500,
+      error: `Internal server error ${error}`,
+    }));
+  };
+
+  
+  //@get a sent message
+    
+    const sentMessage = (req, res) => {
+        const sql = `SELECT * FROM message_table WHERE status = '${req.params.status}'`;
+        const messageSql = Connection.executeQuery(sql);
+        messageSql.then((result) => {
+          if (result.rows.length) {
+            return res.status(200).json({
+              status: 200,
+              data: result.rows,
+            });
+          }
+      
+          return res.status(404).json({
+            status: 404,
+            error: 'No sent email found',
+          });
+        }).catch(error => res.status(500).json({
+          status: 500,
+          error: `Internal server error ${error}`,
+        }));
+      };
+
+
+// @get unread Message
+
+const unreadMessage = (req, res) => {
+    const sql = `SELECT * FROM message_table WHERE status = '${req.params.status}'`;
+    const messageSql = Connection.executeQuery(sql);
+    messageSql.then((result) => {
+      if (result.rows.length) {
+        return res.status(200).json({
+          status: 200,
+          data: result.rows,
+        });
+      }
+  
+      return res.status(404).json({
+        status: 404,
+        error: 'No unread email found',
+      });
+    }).catch(error => res.status(500).json({
+      status: 500,
+      error: `Internal server error ${error}`,
+    }));
+  };
+
+
+ //@deleteEmail
+
+const deleteEmail = (req, res) => {
+
+    const id = req.params.id;
+    Connection.query("SELECT * FROM message_table WHERE id=$1", [id],
+      (error, result) => {
+        if (error) {
+        //console.log(error);
+          return res.status(500).json(error);
+        }
+        if (result.rows.length === 0) {
+          return res.status(400).json({ error: "Sorry! no message found on this id." });
+        }
+        //@delete if it is available
+        pool.query("DELETE FROM meetup_table WHERE id=$1", [id],
+          (er, messageSql) => {
+            if (er) {
+              return res.status(500).json(er);
+            }
+            if (!messageSql) {
+              return res.status(500).json({ error: "something went wrong try again later" });
+            }
+            return res.status(200).json({ success: true, message: "you deleted a message successfully." });
+          });
+      });
+  };
+
+
+    export{
+        getMessages,createMessage,specificEmail,sentMessage,unreadMessage,deleteMessage
+      };
