df04bb2c7281a20c6fedbe436bbb3e87bfe7245d
EmyRukundo <rukundoemma@gmail.com>
2019-03-15 16:23:38 +0200

    deploy aapp on heroku

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ import messageRouter from './server/routes/message';
 import specificRouter from './server/routes/specificEmail';
 import deleteRouter from './server/routes/deleteMessage';
 import groupsRouter from './server/routes/groups';
-import Database from './server/db/db-connection';
+import swagger from 'swagger-ui-express';
+import yamljs from 'yamljs';
+
+const swaggerDocumment = yamljs.load('server/swagger/document.yml');
 
 const app = express();
-
+app.use('/doc',swagger.serve, swagger.setup(swaggerDocumment));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
