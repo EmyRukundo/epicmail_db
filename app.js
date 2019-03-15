6d73227fe3a3ee2b4f284af4b8d06142658b3e7b
EmyRukundo <rukundoemma@gmail.com>
2019-03-15 17:27:29 +0200

    update heroku

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ import messageRouter from './server/routes/message';
 import specificRouter from './server/routes/specificEmail';
 import deleteRouter from './server/routes/deleteMessage';
 import groupsRouter from './server/routes/groups';
-import Database from './server/db/db-connection';
+
 
 const app = express();
+
+
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -34,9 +36,10 @@ app.get((err,req,res,next) => {
 	res.status(404).send('This is not the page you are looking for');
 });
 
-
-
 const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`Listening on port ${port}...`));
 
+
+
 export default app;
+
